cdc344392ba3fcb16f9483b315007affb9193e21
ValdecarlosLZ <lzxbrgamer2@gmail.com>
2023-03-02 20:05:41 -0400

    .

---
+++ processo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <?php
+    echo "<p><h2>Login executado com sucesso! </h2></p><hr>";
+    echo "Usuário: " .$_GET['user']."<hr>";
+    echo "Senha: " .$_GET['password']."<hr>";
+    ?>
+</body>
+</html>
