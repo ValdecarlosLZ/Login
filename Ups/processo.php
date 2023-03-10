7c2e660487957f3d8c100c4f2e76574cec22aa19
ValdecarlosLZ <101909600+ValdecarlosLZ@users.noreply.github.com>
2023-03-10 09:08:41 -0400

    .

--- Ups/processo.php
+++ Ups/processo.php
@@ -1,6 +1,19 @@
 <?php
 
+$cookie_name = "Caos";
+$cookie_value = "36242400";
+setcookie($cookie_name, $cookie_value,time() + 86400);
+
+
+if(isset($_COOKIE[$cookie_name])){
+    echo 'Você de novo???';
+}else{
+    echo 'Quem é você?';
+    setcookie('Caos', 'senha', time() + 360);
+}
+
 // $_SESSION 
+
 
 $user = $_POST["user"];
 $password = $_POST["password"];
